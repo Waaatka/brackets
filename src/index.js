99e875d1271cb13e90529047488ed2c946aeeab7
Anna Sapegina <watochka@gmail.com>
2021-08-28 16:34:57 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const getBracket = (char) => {
+    const index = bracketsConfig.findIndex((x) => x.includes(char));
+    const innerArray = bracketsConfig[index];
+    return [
+      index,
+      innerArray.findIndex((x) => x === char),
+      innerArray[0] === innerArray[1],
+    ];
+  };
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const [index, type, isSame] = getBracket(str[i]);
+    if (type === 0 && (!isSame || stack[stack.length - 1] !== index)) {
+      stack.push(index);
+    } else if (type === 1 || isSame) {
+      if (stack[stack.length - 1] === index) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
